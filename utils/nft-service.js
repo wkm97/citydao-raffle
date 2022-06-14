8a9fa413806813dd53a62dc8aa7d1b7c167d5d5e
karming.wan <31875755+wkm97@users.noreply.github.com>
2022-06-14 23:44:18 +0800

    chore: added github action ci (#2)
    * chore: added github action ci
    
    * chore: added github action for raffle

--- utils/nft-service.js
+++ utils/nft-service.js
@@ -10,6 +10,7 @@ const abi = JSON.parse(fs.readFileSync(abiPath))
 export const provider = new getDefaultProvider("homestead", {
     alchemy: process.env.ALCHEMY_API,
 });
+
 const NFTAddress = "0x7eef591a6cc0403b9652e98e88476fe1bf31ddeb"
 
 
