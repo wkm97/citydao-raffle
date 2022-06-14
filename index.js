128c0a589f76d5ef3d8dce3b6e46efb8d6372aa8
karming.wan <31875755+wkm97@users.noreply.github.com>
2022-06-14 23:29:59 +0800

    test: added integration tests (#1)
    * test: added integration tests
    
    * chore: tidy comments

--- index.js
+++ index.js
@@ -1,57 +1,14 @@
-import { getEthAddressByEns } from './utils/ens-service.js'
-import { balanceOf } from './utils/nft-service.js'
-import { isVoteExists } from './utils/snapshot-service.js'
 import { parse } from 'csv-parse/sync';
-import ethers from "ethers";
 import fs from 'fs'
-import dotenv from 'dotenv'
+import {raffle} from './utils/raffle.js';
 
-dotenv.config();
-
-const TOTAL_WINNERS = 125
-const OUTPUT_PATH = 'output.csv'
-
-const toValidAddress = async (candidate) => {
-    if (ethers.utils.isAddress(candidate)) {
-        return candidate
-    }
-    return getEthAddressByEns(candidate)
-}
-
-const isWinner = async (address) => {
-    // console.log(isVoteExists(address))
-    if (!await isVoteExists(address)) {
-        return false;
-    }
-    if (await balanceOf(address) < 1) {
-        return false;
-    }
-    return true;
-}
+const TOTAL_WINNERS = 125;
 
 async function main() {
     const content = fs.readFileSync("./input.csv")
     const allAddresses = parse(content, { delimiter: ",", from_line: 2 }).map(record => record[0])
-    const uniqAddresses = [...new Set(allAddresses)];
-    const winners = [];
-    while (winners.length < TOTAL_WINNERS) {
-        const randomIdx = Math.floor(Math.random() * uniqAddresses.length)
-        const candidate = uniqAddresses[randomIdx];
-        try {
-            const candidateAddress = await toValidAddress(candidate);
-            const isCandidateWinner = await isWinner(candidateAddress);
-            if (isCandidateWinner) {
-                winners.push(candidate);
-                console.log('Winner:', candidate)
-            } else {
-                console.log('No Condition:', candidate)
-            }
-        }catch(e){
-            console.error(e)
-        }
-        uniqAddresses.splice(randomIdx, 1);
-    }
+    const winners = await raffle(allAddresses, TOTAL_WINNERS);
     fs.writeFileSync(OUTPUT_PATH, winners.join('\n'))
-};
+}
 
 main()
---
+++ tests/integration/raffle.spec.js
@@ -0,0 +1,41 @@
+import { raffle } from '../../utils/raffle.js';
+import assert from 'assert';
+
+const invalidCandidates = ['0x4fc9334d3d78f6f3d7b173d66b0437d225edc3e3', '0x88dafbe57f9e8379dd611cd4fef01d300834c010', 'somerandomstring', 'something.eth', 'invalid.eth'];
+
+describe('raffle', ()=>{
+    const validRuns = [
+        // citizen
+        {candidates: ['feelslike.eth', 'andreiluca.eth', '0x058d968e6a424F72DF07269729f4b8065D213c70','0xede8d0b12d74a32a50fcefc4601527843dcce345'], description: 'citizen with vote'}, 
+        // founding citizen
+        {candidates: ['scofi.eth', 'communitys.eth', 'yiction.eth', '0x57c2cdfe81512f272d5994565e21cb6b5ad4d921'], description: 'founding citizen with vote'}, 
+    ];
+
+    validRuns.forEach((run)=>{
+        it(`valid candidates: ${run.description}`, async()=>{
+            const allCandidates = [...run.candidates, ...invalidCandidates];
+            const results = await raffle(allCandidates);
+            assert.equal(results.length, run.candidates.length); // all sample will be valid
+        })
+    })
+
+    const invalidRuns = [
+        // not owning any citizen
+        {candidates: ['0xc5a61d56ca395fcb87f148532369595e31286d0e', '0x86824c15b825c60bf920f076e8820ebd2ef8c7ad'], description: 'without any nft'},
+        // no vote but with citizen nft
+        {candidates: ['nathcohen.eth', 'binance30.eth', 'apadova.eth', '0x1993d075bb97F0Fe245cD3eA1694bee068466e09', '0x2Ae6332E4a51B499632683279752Effffa5F157F'], description: 'without vote but with citizen nft'}, 
+        // no vote but with founding citizen
+        {candidates: ['0x60e7343205C9C88788a22C40030d35f9370d302D', '0xd576cceD4648d3Fe5e0276c0E698D0d900977A1B', '0xc9F2Fbe0899dEFc450203CEF2Fd05713E250CB48', 'gallery.dfmerin.eth', 'space-lord.eth'], description: 'without vote but with founding citizen'}, 
+        // first citizen but not voted
+        {candidates: ['0xaDB2666f2d9d7A247626925315Da9B54bB34Db7D'], description: 'without vote but with first citizen'} 
+    ]
+    invalidRuns.forEach((run)=>{
+        it(`invalid candidates: ${run.description}`, async()=>{
+            const allCandidates = [...run.candidates];
+            const results = await raffle(allCandidates);
+            assert.equal(results.length, 0); // All invalid candidate, no winners
+        })
+    })
+
+    
+})
