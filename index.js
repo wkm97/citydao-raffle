bf4ad0856177d0a7810be17023a925b03617863b
karming.wan <karming1997@gmail.com>
2022-06-15 23:14:37 +0800

    feat: added cli

--- index.js
+++ index.js
@@ -1,15 +1,26 @@
+#!/usr/bin/env node
+
 import { parse } from 'csv-parse/sync';
 import fs from 'fs'
-import {raffle} from './utils/raffle.js';
+import { raffle } from './utils/raffle.js';
+import _yargs from 'yargs';
+import { hideBin } from 'yargs/helpers';
+const yargs = _yargs(hideBin(process.argv));
 
-const TOTAL_WINNERS = 125;
-const OUTPUT_PATH = 'output.csv';
 
-async function main() {
-    const content = fs.readFileSync("./input.csv")
+(async () => {
+    const argv = await yargs
+        .option('input', { type: 'string', require: true, default: "input.csv"})
+        .option('output', { type: 'string', require: true, default: "output.csv" })
+        .option('winners', { type: 'number', require: false, default: 125})
+        .alias('i', 'input')
+        .alias('o', 'output')
+        .alias('w', 'winners')
+        .argv;
+
+    console.log(`Start raffle from ${argv.input}, total winner: ${argv.winners}`)
+    const content = fs.readFileSync(argv.input)
     const allAddresses = parse(content, { delimiter: ",", from_line: 2 }).map(record => record[0])
-    const winners = await raffle(allAddresses, TOTAL_WINNERS);
-    fs.writeFileSync(OUTPUT_PATH, winners.join('\n'))
-}
-
-main()
+    const winners = await raffle(allAddresses, argv.winners);
+    fs.writeFileSync(argv.output, winners.join('\n'))
+})();
